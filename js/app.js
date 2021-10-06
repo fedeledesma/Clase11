2c6d0f703bec3f115d9036318464155dfe59d138
fedeledesma <mithlond_lnr@live.com.ar>
2021-10-06 10:12:20 -0300

    javaclase16

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*
+console.log("Buenos dias")
+document.write("<h1 class='text2'>Buenos dias</h1>")
+let maxi="chau"
+let maxi2="buenos dias"
+console.log(maxi)
+maxi="hasta mañana"
+console.log(maxi)
+console.log(maxi2)
+const PI= 3.14
+console.log(PI)
+let numero1= 45
+console.log(numero1)
+numero1= 50
+console.log(numero1)
+const MES="julio"
+console.log(MES)
+*/
+
+let num1 = 34
+let num2 = 23
+let num3 = 12
+let num4 = 2
+let sum = num1 + num2
+let rest= num
